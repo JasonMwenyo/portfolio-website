8cca5e4d5f8d7c04d79cca2f6eac365d74ad9043
JasonMwenyo <jasonmwenyo@gmail.com>
2023-01-18 06:47:36 +0200

    first commit

---
+++ src/Components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <div>
+                <img src='../images/hero-image.jpg' />
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
